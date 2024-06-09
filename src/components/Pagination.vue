260d49aca39392d9afd172ab3a312717f499286f
Anna Golmakova <ann.golmakova@gmail.com>
2024-06-09 21:18:44 +0100

    finished pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,7 @@
 <script setup>
-defineProps({
+import { ref, onBeforeUpdate, onMounted, computed } from 'vue'
+
+const props = defineProps({
   currentPage: {
     type: Number,
     required: true
@@ -7,22 +9,42 @@ defineProps({
   totalPages: {
     type: Number,
     required: true
+  },
+  maxVisiblePages: {
+    type: Number,
+    default: 10
   }
+})
+
+const pages = computed(() => {
+  const result = []
+  const visiblePages = Math.min(props.maxVisiblePages, props.totalPages)
+  const start = Math.max(1, Math.ceil(props.currentPage - visiblePages / 2))
+
+  for (let i = start; i <= Math.min(start + visiblePages - 1, props.totalPages); i++) {
+    result.push(i)
+  }
+  return result
 })
 </script>
 
 <template>
   <nav role="navigation" aria-label="pagination" class="pagination">
     <ul class="pagination__container">
+      <li v-if="props.currentPage > maxVisiblePages / 2 + 1" class="pagination__item">
+        <button type="button" @click="$emit('changePage', 1)" class="pagination-button">1</button>
+      </li>
+      <li v-if="props.currentPage > maxVisiblePages / 2 + 2">•••</li>
       <li
-        v-for="index in totalPages"
+        v-for="index in pages"
         :key="index"
-        :class="['pagination__item', index == currentPage ? 'pagination__item_active' : '']"
+        :class="['pagination__item', index == props.currentPage ? 'pagination__item_active' : '']"
       >
         <button type="button" @click="$emit('changePage', index)" class="pagination-button">
           {{ index }}
         </button>
       </li>
+      <li v-if="pages[pages.length - 1] < props.totalPages">•••</li>
     </ul>
   </nav>
 </template>
@@ -32,7 +54,8 @@ defineProps({
   list-style-type: none;
   display: flex;
   flex-direction: row;
-  align-items: flex-start;
+  justify-content: flex-start;
+  align-items: center;
   gap: 8px;
   padding: 0;
 }
